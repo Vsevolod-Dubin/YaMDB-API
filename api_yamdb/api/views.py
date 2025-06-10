2cd6009b3d03211214de5a9f33b02ffdc95b9b1d
Vsevolod-Dubin <vcevolodstarpro@gmail.com>
2025-06-10 17:18:17 +0700

    отработаны все комментарии касающиеся 3 разработчика

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,9 @@
 from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.shortcuts import get_object_or_404
+from django.db import transaction
 
 from api.filters import TitleFilter
 from api.permissions import TitlePermission
@@ -9,8 +12,11 @@ from api.serializers import (
     GenreSerializer,
     TitleCreateSerializer,
     TitleSerializer,
+    CommentSerializer,
+    ReviewSerializer
 )
-from reviews.models import Category, Genre, Title
+from reviews.models import Category, Genre, Title, Review, Title
+from users.permissions import IsAuthorOrModeratorOrAdminOrReadOnly
 
 
 class CreateDestroyViewSet(
@@ -51,8 +57,65 @@ class TitleViewSet(viewsets.ModelViewSet):
     filterset_class = TitleFilter
     permission_classes = (TitlePermission,)
     http_method_names = ["get", "post", "delete", "patch"]
+    lookup_field = "id"
 
     def get_serializer_class(self):
         if self.action in ("list", "retrieve"):
             return TitleSerializer
         return TitleCreateSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для работы с отзывами.
+    Позволяет создавать, читать, обновлять и удалять отзывы.
+    """
+
+    serializer_class = ReviewSerializer
+    permission_classes = [
+        IsAuthenticatedOrReadOnly,
+        IsAuthorOrModeratorOrAdminOrReadOnly,
+    ]
+    http_method_names = ["get", "post", "patch", "delete"]
+    lookup_field = "id"
+
+    def get_title(self):
+        return get_object_or_404(Title, id=self.kwargs.get("title_id"))
+
+    def get_queryset(self):
+        title = self.get_title()
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title = self.get_title()
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Вьюсет для работы с комментариями к отзывам.
+    Позволяет создавать, читать, обновлять и удалять комментарии.
+    """
+
+    serializer_class = CommentSerializer
+    permission_classes = [
+        IsAuthenticatedOrReadOnly,
+        IsAuthorOrModeratorOrAdminOrReadOnly,
+    ]
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    lookup_field = "id"
+
+    def get_review(self):
+        return get_object_or_404(
+            Review,
+            id=self.kwargs.get("review_id"),
+            title__id=self.kwargs.get("title_id")
+        )
+
+    def get_queryset(self):
+        review = self.get_review()
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = self.get_review()
+        serializer.save(author=self.request.user, review=review)
