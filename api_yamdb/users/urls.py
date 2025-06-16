77c09aec400d3a6acffa7f5c1a4f3100ee9f3ba8
pahomdze <pahomdze@gmail.com>
2025-06-16 10:49:58 +0700

    Update urls.py

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,15 +1,7 @@
 from django.urls import include, path
-<<<<<<< HEAD
-
 from rest_framework.routers import DefaultRouter
 
 from .views import AuthViewSet, UserViewSet
-
-=======
-from rest_framework.routers import DefaultRouter
-
-from .views import AuthViewSet, UserViewSet
->>>>>>> origin/develop
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="users")
